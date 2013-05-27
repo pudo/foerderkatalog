bcb839dc2593fa988b7c635101daeb9d3d6cd769
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-27 22:09:31 +0200

    Cleanups

--- datawire.py
+++ datawire.py
@@ -8,7 +8,7 @@ URL_BASE = "http://foerderportal.bund.de/foekat/jsp/SucheAction.do?actionMode=vi
 def submit_all():
     stringer = DataStringer(host='http://localhost:5000', service='foerderkatalog', event='project')
     table = engine['foerderungen']
-    for row in list(table):
+    for row in list(table.find(datawire_submitted=False)):
         if 'datawire_submitted' in row:
             del row['datawire_submitted']
         row['source_url'] = URL_BASE % urllib.quote_plus(row['fkz'])
